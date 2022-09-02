86e27777ee06d10f83f64f919e295e2aab2bb6ac
Ragas Imger <regmis851@gmail.com>
2022-09-02 22:30:37 +0545

    Configured routing settings for the chat endpoint

--- apps/authentication/staticstuffs.py
+++ apps/authentication/staticstuffs.py
@@ -1,3 +1,10 @@
 domain_name = "http://localhost:8000"
 
 default_image = 'profile/photos/default.png'
+
+domain_name_ = "https://localhost:8000"
+
+domian_name_list = [
+    domain_name,
+    domain_name_
+]
--- apps/chat/routing.py
+++ apps/chat/routing.py
@@ -5,5 +5,4 @@ from apps.chat.consumers import ChatConsumer, GroupChatConsumer
 websocket_urlpatterns = [
     path('ws/chat/', ChatConsumer.as_asgi()),
     path('ws/group-chat/', GroupChatConsumer.as_asgi()),
-
 ]
